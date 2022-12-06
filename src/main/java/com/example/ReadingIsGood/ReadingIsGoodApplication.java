0a856a7313c1e9683b943378182acf278b6aee12
eliftultay <eliftultay@gmail.com>
2022-12-06 15:39:57 +0300

    Create controllers

--- src/main/java/com/example/ReadingIsGood/ReadingIsGoodApplication.java
+++ src/main/java/com/example/ReadingIsGood/ReadingIsGoodApplication.java
@@ -2,8 +2,9 @@ package com.example.ReadingIsGood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.ReadingIsGood")
 public class ReadingIsGoodApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/ReadingIsGood/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package com.example.ReadingIsGood.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+
+
+}
